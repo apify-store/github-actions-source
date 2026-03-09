180a3972402e59e0cac1a722a4a10a40e3d3ec0b
metalwarrior665 <lukaskrivka@gmail.com>
2026-03-09 12:09:44 +0100

    chore: remove obsolete src and update readme

--- dist/push-release-latest.d.ts
+++
@@ -1,3 +0,0 @@
-import type { ActorConfig } from './types.js';
-import { StateController } from './state.js';
-export declare const pushToBuildLatest: (actorConfigs: ActorConfig[], stateController: StateController) => Promise<void>;
